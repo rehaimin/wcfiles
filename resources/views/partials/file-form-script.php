53f361503ee3473e60422c113fde6a775dce58cc
Mohammed REHAIMIN <97224542+rehaimin@users.noreply.github.com>
2023-12-05 06:24:43 +0100

    Update file-form-script.php

--- resources/views/partials/file-form-script.php
+++ resources/views/partials/file-form-script.php
@@ -28,4 +28,35 @@
   function decodeURL(url) {
     return decodeURIComponent(url);
   }
+
+    let display = document.querySelector("#timer");
+
+  function startTimer(duration, display) {
+    let timer = duration,
+      minutes,
+      seconds;
+
+    setInterval(function() {
+      minutes = parseInt(timer / 60, 10);
+      seconds = parseInt(timer % 60, 10);
+
+      minutes = minutes < 10 ? "0" + minutes : minutes;
+      seconds = seconds < 10 ? "0" + seconds : seconds;
+
+      display.textContent = minutes + ":" + seconds;
+
+      // if (--timer < 0) {
+      //   timer = duration;
+      // }
+      ++timer
+    }, 1000);
+  }
+
+  let submitBtn = document.querySelector('#submitBtn');
+
+  let twentyMinutes = 60 * 20; // Change this value to set the desired timer duration
+
+  submitBtn.addEventListener('click', function() {
+    startTimer(0, display);
+  });
 </script>
